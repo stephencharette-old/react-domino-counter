8b2e1923946d28183427eeebad872326604ba035
Stephen Charette <stephencharette@Stephens-Air.fios-router.home>
2022-06-12 22:03:34 -0400

    added buttons to add score and scoreboard to display total

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+// Importing components
+import Scorecard from './components/Scorecard.js';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Scorecard total={count} />
+      {[...Array(10)].map((x, i) =>
+        <button onClick={() => setCount(count + (i + 1))}>{i + 1}</button>
+      )}
+      <button onClick={() => setCount(0)}>Clear</button>
     </div>
   );
 }
---
+++ src/components/Scorecard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scorecard = (props) => {
+  return (
+    <header>
+      <h1>{props.total}</h1>
+    </header>
+  );
+}
+
+export default Scorecard;
